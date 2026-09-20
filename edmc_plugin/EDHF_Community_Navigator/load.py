73bd935f85dd22d95efd70d76886c9c74dcd41b7
LittleJacket99 <148579066+LittleJacket99@users.noreply.github.com>
2026-09-20 18:30:04 +0200

    release(edmc): set Companion version to 1.0.0

--- edmc_plugin/EDHF_Community_Navigator/load.py
+++ edmc_plugin/EDHF_Community_Navigator/load.py
@@ -21,7 +21,7 @@ from EDHF_Community_Navigator.rhinospotter_client import (
 )
 
 
-VERSION = "0.1.0"
+VERSION = "1.0.0"
 PLUGIN_NAME = "Hotspots Finder Deposits Companion"
 WORKER_EVENT = "<<EDHFCommunityNavigatorWorker>>"
 NAV_X_KEY = "edhf_community_navigator_x"
